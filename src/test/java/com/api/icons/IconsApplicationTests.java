c00539d42e600707ecd5bd3bc5b34fa5392ff636
DanielDiBartoloit <danieldibartoloit@gmail.com>
2022-03-15 12:33:30 -0300

    Initial commit

---
+++ src/test/java/com/api/icons/IconsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.icons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IconsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
